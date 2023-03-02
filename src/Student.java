fb7146bf9c51129f397eefb4e6ae899f1583fe05
Angel Gabriel <alquisira2004@gmail.com>
2023-03-02 07:56:32 -0600

    Se crea el proyecto minombre

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+
+    private String name;
+    private String lastName;
+    private Long controlNumber;
+    private String career;
+    private int Semester;
+
+
+    public String getName() {return name;}
+
+    public String getLastName() {return lastName;}
+
+    public Long getControlNumber() { return controlNumber;}
+
+    public String getCareer() {return career;}
+
+    public int getSemester() {return Semester;}
+
+
+    public void setName(String name) {this.name = name;}
+
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public void setControlNumber(Long controlNumber) {this.controlNumber = controlNumber;}
+
+    public void setCareer(String career) {this.career = career;}
+
+    public void setSemester(int semester) {Semester = semester;}
+
+
 }
--- src/TestStudent.java
+++ src/TestStudent.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class TestStudent {
+public class TestStudent
+{
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Angel Gabriel");
+        System.out.println("Nombre:"+student.getName());
+
+        student.setLastName("Salgado Alquisira");
+        System.out.println("Apellidos:"+student.getLastName());
+
+        student.setControlNumber(Long.valueOf("22670092"));
+        System.out.println("Numero de control:"+student.getControlNumber());
+
+        student.setCareer("Ingenieria Informatica");
+        System.out.println("Carrera:"+student.getCareer());
+
+        student.setSemester(Integer.parseInt("2"));
+        System.out.println("Semestre:"+student.getSemester());
+        
+    }
 }
